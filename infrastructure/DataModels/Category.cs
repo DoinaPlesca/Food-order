77efb276ccf3c96c0eec81354b11e05bb710f93a
Doina Plesca <doinitzaplesca@yahoo.com>
2023-12-19 10:09:48 +0100

    steel not work

--- infrastructure/DataModels/Category.cs
+++ infrastructure/DataModels/Category.cs
@@ -2,9 +2,9 @@
 
 public class Category
 {
-    public int CategoryId { get; set; }
-    public string CategoryName { get; set; }
-    public string CategoryImageUrl { get; set; }
+    public int? CategoryId { get; set; }
+    public string? CategoryName { get; set; }
+    public string? CategoryImageUrl { get; set; }
     
     
 }
--- infrastructure/DataModels/Product.cs
+++ infrastructure/DataModels/Product.cs
@@ -5,13 +5,13 @@ namespace infrastructure.DataModels;
 public class Product
 {
 
-    public int ProductId{ get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
-    public string ImageUrl { get; set; }
-    public int CategoryId { get; set; }
+    public int? ProductId{ get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal? Price { get; set; }
+    public int? Quantity { get; set; }
+    public string? ImageUrl { get; set; }
+    public int? CategoryId { get; set; }
     public Category Category { get; set; }
     
 }
