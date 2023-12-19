cc9d0f5b3478605b8faa51f42c0619cb3fd7b2aa
Doina Plesca <doinitzaplesca@yahoo.com>
2023-12-19 09:37:45 +0100

    try solve the error

--- infrastructure/DataModels/User.cs
+++ infrastructure/DataModels/User.cs
@@ -3,11 +3,11 @@
 public class User
 {
     public int id { get; set; }
-    public string username { get; set; }
-    public string email { get; set; }
-    public string password { get; set; }
-    public string salt { get; set; }
+    public string? username { get; set; }
+    public string? email { get; set; }
+    public string? password { get; set; }
+    public string? salt { get; set; }
     
-    public string algorithm { get; set; }
-    public string role { get; set; }
+    public string? algorithm { get; set; }
+    public string? role { get; set; }
 }
